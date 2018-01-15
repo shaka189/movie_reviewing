74194fe5b47728c187223154f14f68782ba349ef
huuphuc93 <huuphuc.ns@gmail.com>
2018-01-15 19:35:04 +0700

    fix layout index admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,12 @@
 class Admin::AdminController < Admin::BaseController
   def index
+    @new_users = User.new_users.desc_create_time.paginate page: params[:page],
+      per_page: Settings.paginate_number.per_page
+    @new_post_reviews = PostReview.new_post_reviews.newest.paginate page: params[:page],
+      per_page: Settings.paginate_number.per_page
+    @new_bookings = Booking.new_bookings.desc_create_time.paginate page: params[:page],
+      per_page: Settings.paginate_number.per_page
+    @new_comments = Comment.new_comments.desc_create_at.paginate page: params[:page],
+      per_page: Settings.paginate_number.per_page
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post_review
   scope :desc_create_at, -> {order("created_at desc")}
   validates :content, presence: true
+  
+  def self.new_comments
+    where("created_at > ?", Time.new.beginning_of_day)
+  end
 end
